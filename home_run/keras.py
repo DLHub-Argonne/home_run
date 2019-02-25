9812c2dd7fbb49ce3fc2f0d024426dd01ff85865
Logan Ward <ward.logan.t@gmail.com>
2019-02-25 12:58:28 -0600

    Added support for multifile models and custom layers

--- home_run/keras.py
+++ home_run/keras.py
@@ -1,5 +1,6 @@
 from .base import BaseServable
-from keras.models import load_model
+from keras.models import load_model, model_from_yaml, model_from_json
+from importlib import import_module
 import numpy as np
 
 
@@ -7,8 +8,41 @@ class KerasServable(BaseServable):
     """Servable based on a Keras Model"""
 
     def _build(self):
+        # Get all of the custom objects
+        if 'options' in self.servable and 'custom_objects' in self.servable['options']:
+            custom_objects = dict(self.servable['options']['custom_objects'])
+
+            # Import each of the described classes
+            for k, v in custom_objects.items():
+                temp = v.split(".")
+                mod = import_module('.'.join(temp[:-1]))
+                cls = getattr(mod, temp[-1])
+                custom_objects[k] = cls
+        else:
+            custom_objects = None
+
         # Load in the model from disk
-        self.model = load_model(self.dlhub['files']['model'])
+        if 'arch' in self.dlhub['files']:
+            # Load in the architecture
+            arch_path = self.dlhub['files']['arch']
+            if arch_path.endswith('.h5') or arch_path.endswith('.hdf') \
+                    or arch_path.endswith('.hdf5') or arch_path.endswith('.hd5'):
+                self.model = load_model(arch_path, custom_objects=custom_objects, compile=False)
+            elif arch_path.endswith('.json'):
+                with open(arch_path) as fp:
+                    json_string = fp.read()
+                self.model = model_from_json(json_string, custom_objects=custom_objects)
+            elif arch_path.endswith('.yml') or arch_path.endswith('.yaml'):
+                with open(arch_path) as fp:
+                    yaml_string = fp.read()
+                self.model = model_from_yaml(yaml_string, custom_objects=custom_objects)
+            else:
+                raise ValueError('File type for architecture not recognized')
+
+            # Load in the weights
+            self.model.load_weights(self.dlhub['files']['model'])
+        else:
+            self.model = load_model(self.dlhub['files']['model'])
 
         # Check whether this model is a multi-input
         self.is_multiinput = self.servable['methods']['run']['input']['type'] == 'tuple'
--- tests/test_keras.py
+++ tests/test_keras.py
@@ -10,14 +10,19 @@ import os
 from home_run.keras import KerasServable
 
 
+def _make_model():
+    model = Sequential()
+    model.add(Dense(16, input_shape=(1,), activation='relu', name='hidden'))
+    model.add(Dense(1, name='output'))
+    model.compile(optimizer='rmsprop', loss='mse')
+    return model
+
+
 class KerasTest(TestCase):
 
     def test_keras(self):
         # Make a Keras model
-        model = Sequential()
-        model.add(Dense(16, input_shape=(1,), activation='relu', name='hidden'))
-        model.add(Dense(1, name='output'))
-        model.compile(optimizer='rmsprop', loss='mse')
+        model = _make_model()
 
         # Save it
         tempdir = mkdtemp()
@@ -96,3 +101,73 @@ class KerasTest(TestCase):
             self.assertTrue(np.isclose(model.predict(np.array(x)), servable.run(x)).all())
         finally:
             shutil.rmtree(tempdir)
+
+    def test_keras_multifile(self):
+        """Test loading a shim when model is saved as a separate arch and weights file"""
+
+        # Make a Keras model
+        model = _make_model()
+
+        # Save it
+        tempdir = mkdtemp()
+        try:
+            model_path = os.path.join(tempdir, 'model.hd5')
+            model.save(model_path, include_optimizer=False)
+            model_json = os.path.join(tempdir, 'model.json')
+            with open(model_json, 'w') as fp:
+                print(model.to_json(), file=fp)
+            model_yaml = os.path.join(tempdir, 'model.yml')
+            with open(model_yaml, 'w') as fp:
+                print(model.to_yaml(), file=fp)
+
+            weights_path = os.path.join(tempdir, 'weights.hd5')
+            model.save_weights(weights_path)
+
+            # Create the metadata
+            metadata = KerasModel.create_model(weights_path, ['y'], arch_path=model_path)
+            metadata.set_title('Keras Test')
+            metadata.set_name('mlp')
+
+            # Make the servable
+            servable = KerasServable(**metadata.to_dict())
+            x = [[1]]
+            self.assertAlmostEqual(model.predict(np.array(x))[0],
+                                   servable.run(x)[0])
+
+            # Test with the JSON and YAML
+            metadata = KerasModel.create_model(weights_path, ['y'], arch_path=model_json)
+            metadata.set_title('Keras Test').set_name('mlp')
+            servable = KerasServable(**metadata.to_dict())
+            self.assertAlmostEqual(model.predict(np.array(x))[0], servable.run(x)[0])
+
+            metadata = KerasModel.create_model(weights_path, ['y'], arch_path=model_yaml)
+            metadata.set_title('Keras Test').set_name('mlp')
+            servable = KerasServable(**metadata.to_dict())
+            self.assertAlmostEqual(model.predict(np.array(x))[0], servable.run(x)[0])
+        finally:
+            shutil.rmtree(tempdir)
+
+    def test_custom_layers(self):
+        """Test adding custom layers to the definition"""
+
+        # Make a simple model
+        model = _make_model()
+
+        tmpdir = mkdtemp()
+        try:
+            # Save it
+            model_path = os.path.join(tmpdir, 'model.hd5')
+            model.save(model_path)
+
+            # Create the metadata
+            metadata = KerasModel.create_model(model_path, ['y'], custom_objects={'Dense': Dense})
+            metadata.set_title('Keras Test')
+            metadata.set_name('mlp')
+
+            # Make the servable
+            servable = KerasServable(**metadata.to_dict())
+            x = [[1]]
+            self.assertAlmostEqual(model.predict(np.array(x))[0],
+                                   servable.run(x)[0])
+        finally:
+            shutil.rmtree(tmpdir)
